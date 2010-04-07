2b0b68fffba9f47186364e013d8cc46924ce5643
Patrick Donelan <pat@patspam.com>
2010-04-07 13:01:07 -0400

    Updated with some new plugins

--- lib/Dist/Zilla/PluginBundle/PDONELAN.pm
+++ lib/Dist/Zilla/PluginBundle/PDONELAN.pm
@@ -7,7 +7,29 @@ use Moose::Autobox;
 with 'Dist::Zilla::Role::PluginBundle';
 
 use Dist::Zilla::PluginBundle::Filter;
+use Dist::Zilla::PluginBundle::Classic;
 use Dist::Zilla::PluginBundle::Git;
+use Dist::Zilla::Plugin::AutoPrereq;
+use Dist::Zilla::Plugin::AutoVersion;
+use Dist::Zilla::Plugin::CheckChangeLog;
+use Dist::Zilla::Plugin::CheckChangesTests;
+use Dist::Zilla::Plugin::CompileTests;
+use Dist::Zilla::Plugin::DistManifestTests;
+use Dist::Zilla::Plugin::HasVersionTests;
+use Dist::Zilla::Plugin::ManifestSkip;
+use Dist::Zilla::Plugin::MetaTests;
+use Dist::Zilla::Plugin::MetaJSON;
+use Dist::Zilla::Plugin::Repository;
+use Dist::Zilla::Plugin::MetaResources;
+use Dist::Zilla::Plugin::MinimumVersionTests;
+use Dist::Zilla::Plugin::ModuleBuild;
+use Dist::Zilla::Plugin::NextRelease;
+use Dist::Zilla::Plugin::PodWeaver;
+use Dist::Zilla::Plugin::PortabilityTests;
+use Dist::Zilla::Plugin::Prepender;
+use Dist::Zilla::Plugin::ReadmeFromPod;
+use Dist::Zilla::Plugin::SynopsisTests;
+use Dist::Zilla::Plugin::UploadToCPAN;
 
 sub bundle_config {
     my ( $self, $section ) = @_;
@@ -19,28 +41,45 @@ sub bundle_config {
         {   name    => "$class/Classic",
             payload => {
                 bundle => '@Classic',
-                remove => [qw(PodVersion Readme)],
+                remove => [qw(PodVersion Readme Manifest)],
             }
         }
     );
 
+    # params for AutoVersion
+    my $major_version = defined $arg->{major_version} ? $arg->{major_version} : 1;
+
+    my %meta_resources;
+    for my $resource qw(homepage bugtracker repository license ratings) {
+        $meta_resources{$resource} = $arg->{$resource} if defined $arg->{$resource};
+    }
+
+    # params
+
     my $prefix = 'Dist::Zilla::Plugin::';
-    my @extra
-        = map { [ "$class/$prefix$_->[0]" => "$prefix$_->[0]" => $_->[1] ] } (
-        [ AutoPrereq     => {} ],
-        [ AutoVersion    => {} ],
-        [ CheckChangeLog => {} ],
-        [ CompileTests   => {} ],
-        [ MetaTests      => {} ],
-        [ MetaJSON       => {} ],
-        [ ModuleBuild    => {} ],
-        [ NextRelease    => {} ],
-        [ PodWeaver      => {} ],
-        [ Prepender      => {} ],
-        [ Repository     => {} ],
-        [ ReadmeFromPod  => {} ],
-        [ UploadToCPAN   => {} ],
-        );
+    my @extra = map { [ "$class/$prefix$_->[0]" => "$prefix$_->[0]" => $_->[1] ] } (
+        [ AutoPrereq          => { skip  => $arg->{auto_prereq_skip} } ],
+        [ AutoVersion         => { major => $major_version } ],
+        [ CheckChangeLog      => {} ],
+        [ CheckChangesTests   => {} ],
+        [ CompileTests        => {} ],
+        [ HasVersionTests     => {} ],
+        [ MetaTests           => {} ],
+        [ MetaJSON            => {} ],
+        [ Repository          => {} ],
+        [ ManifestSkip        => {} ],
+        [ MetaResources       => \%meta_resources ],
+        [ MinimumVersionTests => {} ],
+        [ ModuleBuild         => {} ],
+        [ NextRelease         => {} ],
+        [ PodWeaver           => {} ],
+        [ PortabilityTests    => {} ],
+        [ Prepender           => {} ],
+        [ ReadmeFromPod       => {} ],
+        [ SynopsisTests       => {} ],
+        [ UploadToCPAN        => {} ],
+        [ Manifest            => {} ],   # should come last
+    );
 
     push @plugins, @extra;
 
@@ -63,31 +102,55 @@ no Moose;
 1;
 __END__
 
-=head1 SYNOPSIS
+=head1 DESCRIPTION
 
-    # dist.ini
+Putting the following in your dist.ini file:
+
     [@PDONELAN]
 
-It is equivalent to:
+is equivalent to:
 
     [@Filter]
     bundle = @Classic
     remove = PodVersion
     remove = Readme
+    remove = Manifest
       
     [AutoPrereq]
     [AutoVersion]
     [CheckChangeLog]
+    [CheckChangesTests]
     [CompileTests]
+    [DistManifestTests]
     [@Git]
+    [HasVersionTests]
+    [ManifestSkip]
     [MetaTests]
     [MetaJSON]
+    [Repository]
+    [MetaResources]
+    [MinimumVersionTests]
     [ModuleBuild]
     [NextRelease]
     [PodWeaver]
+    [PortabilityTests]
     [Prepender]
-    [Repository]
     [ReadmeFromPod]
+    [SynopsisTests]
     [UploadToCPAN]
+    [Manifest]
+
+You can specify the following options
+
+    major_version = X ;; passed to AutoVersion (defaults to 1)
+    auto_prereq_skip = ^Foo|Bar$
+    
+And also any of the following MetaResources
+
+    homepage 
+    bugtracker 
+    repository ;; only needed if you want to overwrite [Repository]'s value
+    license 
+    ratings
 
 =cut
